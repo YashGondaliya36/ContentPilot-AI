0c654d88d8bf40c0574962f63bd5c4e703627c26
yashgondaliya36 <yashgondaliya36@gmail.com>
2025-03-10 17:17:09 +0530

    agent added

---
+++ agents.py
@@ -0,0 +1,40 @@
+from crewai import Agent, LLM
+import os
+from dotenv import load_dotenv
+from tools import search_tool
+
+load_dotenv()
+
+# Initialize 
+gemini = LLM(
+    model="gemini/gemini-2.0-flash",
+    api_key=os.getenv('GOOGLE_API_KEY'),
+    max_tokens=4096
+)
+
+# Define agents
+researcher = Agent(
+    role="Researcher",
+    goal="Find useful information",
+    backstory="You find helpful information online",
+    tools=[search_tool],
+    llm=gemini,
+    verbose=True
+)
+
+planner = Agent(
+    role="Planner",
+    goal="Make simple content plans",
+    backstory="You create easy-to-follow content plans",
+    tools=[search_tool],
+    llm=gemini,
+    verbose=True
+)
+
+writer = Agent(
+    role="Writer",
+    goal="Create engaging content",
+    backstory="You write content people enjoy reading",
+    llm=gemini,
+    verbose=True
+) 
